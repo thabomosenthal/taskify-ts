f09d676cd36000be74bc874ed18638822ec91500
Thabo Mosenthal <thabo@mosenthal@hotmail.com>
2023-03-24 10:17:14 +0200

    Squashed commit of the following:
    commit d4969e94f3958d9df04eb39950f850f17d949795
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Wed Mar 22 19:58:30 2023 +0200
    
        adjust style
    
    commit f06774899b3ca64733ffa3030944d7d0881c03a8
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Wed Mar 22 19:53:35 2023 +0200
    
        adjust debugging
    
    commit 88044d4dd25de8791df48f283d5f509650c3796d
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Wed Mar 22 17:47:30 2023 +0200
    
        complete
    
    commit 44e5587a4e22a07fd4e71250324753f88114e0df
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Wed Mar 22 10:55:01 2023 +0200
    
        install react-icons
    
    commit 045a0ebf7a6466149210a07998198846117b2bd9
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Wed Mar 22 10:52:36 2023 +0200
    
        create todolist component
    
    commit 75622d02d28175003a20868015e0eca3c9cf31e5
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Wed Mar 22 08:37:33 2023 +0200
    
        handle key enter submit
    
    commit e187818693ddffb987a506186daf796e81bd9265
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Tue Mar 21 22:56:59 2023 +0200
    
        created basic logic for the form
    
    commit 6739e3f6d2c31a67369e7a5350a9fa191d6cc6d9
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Tue Mar 21 22:46:55 2023 +0200
    
        add handle add task
    
    commit 89107dd11930733ba7cdc344987237e16f021925
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Tue Mar 21 22:27:31 2023 +0200
    
        created todo interface model
    
    commit 7925123a182fcaaaaafdbe27f36feda10238b233
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Tue Mar 21 22:17:14 2023 +0200
    
        passed Props to component
    
    commit a512fb0cf70d728407ea1df69644a21d46b8d94e
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Tue Mar 21 21:57:19 2023 +0200
    
        place items top
    
    commit a6008e13a08046a62ab1fd4c1ce7940aa82a01a4
    Author: Thabo Mosenthal <thabo@mosenthal@hotmail.com>
    Date:   Tue Mar 21 21:55:48 2023 +0200
    
        created todo state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,160 @@
-import { FC } from 'react';
+import { Dispatch, FC, FormEvent, SetStateAction, useEffect, useRef, useState } from 'react';
+import { FaCheck, FaRegEdit, FaRegTrashAlt } from 'react-icons/fa';
 import './App.css';
+import { ITodo } from './models/todo';
 
 const App: FC = () => {
+	const [todo, setTodo] = useState<string>('');
+	const [todoList, setTodoList] = useState<ITodo[]>([]);
+
+	// debugging;
+	useEffect(() => {
+		console.log(todoList);
+	}, [todoList]);
+
+	const handleAddTask = (e: FormEvent) => {
+		e.preventDefault();
+
+		if (todo.trim() === '') return;
+		const newTodo: ITodo = {
+			id: new Date().getTime(),
+			title: todo,
+			completed: false,
+		};
+		setTodoList([...todoList, newTodo]);
+		setTodo('');
+	};
+
 	return (
 		<>
-			<main>
-				<h1 className="text-3xl font-bold underline">App</h1>
+			<main className="flex flex-col items-center w-full gap-4 ">
+				<h1 className="text-3xl font-bold z-10">Taskify</h1>
+				<CInputField todo={todo} setTodo={setTodo} handleAdd={handleAddTask} />
+				<CTodoList todoList={todoList} setTodoList={setTodoList} />
 			</main>
+		</>
+	);
+};
+
+// InputField component
+interface IInputFieldProps {
+	todo: string;
+	setTodo: Dispatch<SetStateAction<string>>;
+	handleAdd: (e: FormEvent) => void;
+}
+
+const CInputField = ({ todo, setTodo, handleAdd }: IInputFieldProps) => {
+	// const CInputField: FC<IInputFieldProps> = ({ todo, setTodo }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	return (
+		<>
+			<form
+				className="flex relative min-w-[15em] sm:min-w-[30em] items-center"
+				onSubmit={(e) => {
+					handleAdd(e);
+					inputRef.current?.blur();
+				}}
+			>
+				<input
+					ref={inputRef}
+					type="text"
+					id="task"
+					placeholder="Enter task"
+					value={todo}
+					onChange={(e) => setTodo(e.target.value)}
+					className="px-3 h-14 text-sm w-full rounded-full delay-200 focus:outline-none shadow-inner focus:shadow-[0_0_10px_1000px_rgba(0,0,0,0.1)]"
+				/>
+				<button type="submit" className="rounded-full absolute right-2 aspect-square w-10 p-0 bg-slate-300 dark:bg-slate-700">
+					add
+				</button>
+			</form>
+		</>
+	);
+};
+
+// TodoList component
+interface ITodoListProps {
+	todoList: ITodo[];
+	setTodoList: Dispatch<SetStateAction<ITodo[]>>;
+}
+
+const CTodoList = ({ todoList, setTodoList }: ITodoListProps) => {
+	return (
+		<>
+			{/* <h3>Todo List here...</h3> */}
+			<div className="flex flex-col gap-0.5 py-3">
+				{todoList.length === 0 ? <p>No tasks</p> : todoList.map((todo) => <CTodoItem key={todo.id} todo={todo} todoList={todoList} setTodoList={setTodoList} />)}
+			</div>
+		</>
+	);
+};
+
+// TodoItem component
+interface ITodoItemProps {
+	todo: ITodo;
+	todoList: ITodo[];
+	setTodoList: Dispatch<SetStateAction<ITodo[]>>;
+}
+
+const CTodoItem = ({ todo, todoList, setTodoList }: ITodoItemProps) => {
+	const editableTaskRef = useRef<any>({});
+	const [isEditing, setIsEditing] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (isEditing && !todo.completed) {
+			editableTaskRef.current?.setAttribute('contenteditable', 'true');
+			editableTaskRef.current?.focus();
+		} else {
+			editableTaskRef.current?.setAttribute('contenteditable', 'false');
+			editableTaskRef.current?.blur();
+		}
+	}, [isEditing]);
+
+	const handleEditTask = (todo: ITodo): void => {
+		const editableText = editableTaskRef.current.innerText;
+
+		setIsEditing(!isEditing);
+		setTodoList(
+			todoList.map((item) => {
+				if (item.id === todo.id) return { ...item, title: editableText };
+				return item;
+			})
+		);
+	};
+
+	const handleCompletedTask = (todo: ITodo): void => {
+		setTodoList(
+			todoList.map((item) => {
+				if (item.id === todo.id) return { ...item, completed: !item.completed };
+				return item;
+			})
+		);
+	};
+
+	const handleDeleteTask = (todo: ITodo): void => {
+		setTodoList(todoList.filter((item) => item.id !== todo.id));
+	};
+
+	return (
+		<>
+			<p
+				className={`flex items-center justify-between gap-1 w-52 sm:min-w-[25em] p-2 bg-slate-300 rounded-md ${
+					todo.completed ? ' dark:bg-green-700 dark:border-b-4 dark:border-green-900 ' : ' dark:bg-slate-700 dark:border-b-slate-800 dark:border-b-4 '
+				}`}
+			>
+				<span ref={editableTaskRef} className="flex-grow text-left w-min">
+					{todo.title}
+				</span>
+				<button type="button" title="edit task" onClick={() => handleEditTask(todo)} className="p-0.5 border-none hover:text-gray-600 focus:outline-none focus:text-gray-400">
+					<FaRegEdit />
+				</button>
+				<button type="button" title="mark task completed" onClick={() => handleCompletedTask(todo)} className="p-0.5 border-none hover:text-green-600 focus:outline-none focus:text-green-400">
+					<FaCheck />
+				</button>
+				<button type="button" title="delete task" onClick={() => handleDeleteTask(todo)} className="p-0.5 border-none hover:text-red-600 focus:outline-none focus:text-red-400 ">
+					<FaRegTrashAlt />
+				</button>
+			</p>
 		</>
 	);
 };
